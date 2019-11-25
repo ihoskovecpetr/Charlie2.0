1648b06201d4a7c554d61011e73540954556f443
Petr Hoskovec <petr.hoskovec@styrax.cz>
2019-11-25 16:27:13 +0100

    menu style

--- client/src/Pages/Menu.js
+++ client/src/Pages/Menu.js
@@ -3,13 +3,35 @@ import { NavLink } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import { lightBlue } from "@material-ui/core/colors";
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import CardMedia from '@material-ui/core/CardMedia';
+import Avatar from '@material-ui/core/Avatar';
 
 const useStyles = makeStyles(theme => ({
+  container: {
+    flexGrow: 1,
+    backgroundColor: 'lightBlue'
+  },
   root: {
     padding: theme.spacing(3, 2),
     margin: theme.spacing(3, 2),
-    backgroundColor: "lightGrey"
+    backgroundColor: 'lightBlue'
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  center: {
+    textAlign: 'center',
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    'align-items': 'center'
+  },
+  gridLogo: {
+    textAlign: 'center',
   }
 }));
 
@@ -19,10 +41,38 @@ export default function Menu(props) {
   console.log("Menu props: ", props);
 
   return (
-    <div>
+    <>
+     <Container maxWidth="sm">
+       <Grid container justify='center' direction='column' className={classes.container}>
+        <Grid item justify='center'>
+          <Typography variant="h4" component="h3" className={classes.center}>
+            PROJECT           
+          </Typography>
+          </Grid>
+          <Grid item justify='center'>
+          <Typography variant="h3" component="h3" className={classes.center}>
+            CHARLIE           
+          </Typography>
+        </Grid>
+        
+        <Grid item justify='center'  alignItems="center" >
+        <Grid container justify='center'>
+        
+        <Avatar className={classes.avatar} alt="Remy Sharp" src="https://res.cloudinary.com/party-images-app/image/upload/v1557794256/ojkgl1hkiljwij69njbb.png" />
+        
+        </Grid>
+        </Grid>
+        </Grid>
+        
+        <CardMedia
+        className={classes.media}
+        image="https://res.cloudinary.com/party-images-app/image/upload/v1557794256/ojkgl1hkiljwij69njbb.png"
+        title="Paella dish"
+      />
+       
       {props.ListOfUrls.map((item, index) => (
         <NavLink to={`/${item}`} key={index}>
-          <Paper className={classes.root}>
+          <Paper className={classes.root} >
             <Typography variant="h5" component="h3">
               {props.ListOfNames[index]}
             </Typography>
@@ -33,6 +83,7 @@ export default function Menu(props) {
           </Paper>
         </NavLink>
       ))}
-    </div>
+      </Container>
+    </>
   );
 }
