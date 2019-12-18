336ee4ff4e2c3b815ddced98227961e26b9db3d1
Petr Hoskovec <ihoskovecpetr@gmail.com>
2019-12-18 23:20:54 +0100

    18.12.

---
+++ build/precache-manifest.2f5067d4749b628b0b04db01709df5b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4077d216c3eec08982daeae3df3f55b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c704a0a34eb24336423b",
+    "url": "/static/css/2.0b405c91.chunk.css"
+  },
+  {
+    "revision": "daa5f0010d28f2e19f77",
+    "url": "/static/css/main.66546c69.chunk.css"
+  },
+  {
+    "revision": "c704a0a34eb24336423b",
+    "url": "/static/js/2.bfdf27a3.chunk.js"
+  },
+  {
+    "revision": "daa5f0010d28f2e19f77",
+    "url": "/static/js/main.b520c6e0.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
